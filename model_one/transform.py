d8ca20f6fa0c5db020f1bf7b30a6fb6502e73985
eprin2 <email specjalny od github nie gmail>
2026-04-22 20:11:33 +0200

    upd transform

--- model_one/transform.py
+++ model_one/transform.py
@@ -19,10 +19,40 @@ def get_data(data_file, show=False):
 
 
 
-def raw(inp):
-    data = get_data(inp, show=False)[0]["text"]
+def raw(inp,ind):
+    data = get_data(inp, show=False)[ind]["text"]
     data = re.sub(r'={2,3}', '', data)  # usuń == i ===
     data = data.replace('\n', ' ')      # usuń \n
+    data = data.replace('(', ' ')      # usuń \n
+    data = data.replace(')', ' ')      # usuń \n
     tokens = [n for i in data.split(". ") for n in i.split(" ") if n.strip()]
     return " ".join(tokens)
-print(raw("data.json"))
+def add_token_transform(inp,ind,lists):
+    te=lists[0]
+    ty=lists[1]
+    for i in raw(inp,ind).split(" "):
+        if i in te:
+            pass
+        else:
+             te.append(i)
+             ty.append([])
+    return te,ty
+def token_transform(inp,lists):
+    te=[]
+    ty=[]
+    for i in range(len(inp)):
+        x=add_token_transform(inp,i,lists)
+        te=te+x[0]
+    for i in te:
+        ty.append(5580*[0])
+    return te,ty
+          
+def get_list_dic(inp):
+        da=get_data("dic.json")
+        dic_te=[]
+        dic_ty=[]
+        for i in da:
+             dic_te.append(i["text"])
+             dic_ty.append(i["type"])        
+        return dic_te,dic_ty
+
